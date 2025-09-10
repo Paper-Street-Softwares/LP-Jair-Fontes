e0be30555f94b46aca6f33b1dd2f70304d1d6169
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-09-10 15:38:36 -0300

    fix: Imagens e email atualizados

--- src/components/sectionElements/footer/EmailFooter.jsx
+++ src/components/sectionElements/footer/EmailFooter.jsx
@@ -15,7 +15,7 @@ function EmailFooter({
         <div className="flex full gap-x-[12px] items-center opacity-90">
           <Mail />
 
-          <p className="text-paragraph2">{content.texts.infos.email}</p>
+          <p className="text-paragraph2">jairfontes.adv@hotmail.com</p>
         </div>
       </MotionDivDownToUp>
 
@@ -24,7 +24,9 @@ function EmailFooter({
           <div className="flex full gap-x-[12px] items-center opacity-90">
             <Mail />
 
-            <p className="text-paragraph2">{content.texts.infos.emailSecundario} </p>
+            <p className="text-paragraph2">
+              {content.texts.infos.emailSecundario}{" "}
+            </p>
           </div>
         </MotionDivDownToUp>
       )}
